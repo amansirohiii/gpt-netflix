a8669c5513dcb7295f302fb66f332c0fbfa1ab50
Aman Sirohi <amansirohi077@gmail.com>
2024-01-03 01:38:15 +0530

    added trailer autoreplay

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -10,7 +10,7 @@ const VideoBackground = ({movieId}) => {
     <div className="w-screen">
       <iframe
       className=" w-full aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?&autoplay=1&mute=1&controls=0&showinfo=0&loop=1"}
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key + "?&autoplay=1&mute=1&controls=0&showinfo=0&loop=1&playlist=" +trailerVideo.key }
         title="YouTube video player"
         allow="accelerometer; autoplay=1; mute=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 
