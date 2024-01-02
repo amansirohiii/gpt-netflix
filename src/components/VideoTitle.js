933be9b096705fcddfaa3d71e71512a508780a20
Aman Sirohi <amansirohi077@gmail.com>
2024-01-02 23:19:32 +0530

    added trailer to bg

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,11 +1,11 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-48 px-12">
+    <div className="w-screen aspect-video pt-52 px-12 absolute bg-gradient-to-r from-black text-white">
     <h1 className="text-5xl py-4 font-bold">{title}</h1>
     <p className="py-6 text-lg w-1/2">{overview}</p>
     <div>
-        <button className="px-12 p-4 bg-gray-500 text-lg text-white bg-opacity-50 rounded-md">Play</button>
-        <button className="mx-2 px-12 p-4 bg-gray-500 text-lg text-white bg-opacity-50 rounded-md">More Info</button>
+        <button className="px-12 p-4 bg-white text-lg text-black hover:bg-opacity-80 rounded-md font-bold">Play</button>
+        <button className="mx-2 px-12 p-4 bg-white text-lg text-black hover:bg-opacity-80 rounded-md font-bold">More Info</button>
     </div>
   </div>
   )
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -8,7 +8,7 @@ const useMovieTrailer = (movieId)=>{
     const getMovieVideo = async()=>{
         const data = await fetch("https://api.themoviedb.org/3/movie/"+ movieId + "/videos?language=en-US", API_OPTIONS);
         const json = await data.json();
-        console.log(json);
+        // console.log(json);
 
         const filterData = json.results.filter((video)=>video.type==="Trailer");
         const trailer = filterData.length? filterData[0] : json.results[0];
