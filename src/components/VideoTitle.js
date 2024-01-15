8e476619b2eeafd61d73780d68cf73ea75258b8b
Aman Sirohi <amansirohi077@gmail.com>
2024-01-15 22:05:09 +0530

    Update VideoTitle.js

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,7 +1,7 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className=" aspect-video pt-50%] md:pt-[20%] px-12 absolute bg-gradient-to-r from-black text-white">
-    <h1 className="text-2xl md:text-5xl py-3 md:py-3 font-bold">{title}</h1>
+    <div className=" aspect-video pt-30%] md:pt-[20%] px-12 absolute bg-gradient-to-r from-black text-white">
+    <h1 className="text-2xl md:text-5xl py-1 md:py-3 font-bold">{title}</h1>
     <p className="hidden md:inline-block py-2 md:py-6 text-lg w-1/2 line-clamp-2">{overview}</p>
     <div>
         <button className="hidden md:inline-block px-5 p-2 md:px-12 md:p-4 bg-white text-lg text-black hover:bg-opacity-80 rounded-md font-bold">Play</button>
