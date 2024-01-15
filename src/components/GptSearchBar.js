7b568924fac1d0b4ac029af7f06073e2f65d71e0
Aman Sirohi <amansirohi077@gmail.com>
2024-01-15 22:12:01 +0530

    Update GptSearchBar.js

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -90,7 +90,7 @@ const GptSearchBar = () => {
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
         <button
-          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          className="col-span-3 m-4 py-2 px-2 sm:px-4 bg-red-700 text-white rounded-lg"
           onClick={handleGptSearchClick}
         >
           {moviesLoading ?"Loading.." : lang[langKey].search}
